209fa19026d17749bed451c0ccb9092ab08c6999
bandanishanth <bandanishanth@gmail.com>
2020-06-28 22:33:08 +0530

    Removed Serialization on Student Class

--- src/main/java/com/example/sochdemo/Student.java
+++ src/main/java/com/example/sochdemo/Student.java
@@ -4,10 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 @Entity
-public class Student implements Serializable {
+public class Student
+{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
